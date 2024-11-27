7fa216a7aa801487e2e133544a1a3f96bd1d2cd0
Ábel Miklós <abel.miklos@stud.uni-obuda.hu>
2024-11-27 17:58:33 +0100

    Removed explicit service injection from program.cs and moved to Application dependency injection

---
+++ src/GMYEL8_HSZF_2024251/GMYEL8_HSZF_2024251.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using GMYEL8_HSZF_2024251.Application.Definitions.TaxiCarServices;
+using GMYEL8_HSZF_2024251.Application.Implementations.TaxiCarServices;
+using GMYEL8_HSZF_2024251.Persistence.MsSql.DataProviders.Definitions;
+using GMYEL8_HSZF_2024251.Persistence.MsSql.DataProviders.Implementations;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GMYEL8_HSZF_2024251.Application
+{
+    public static class DependencyInjection
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddSingleton<ITaxiCarServiceDataProvider, TaxiCarDataProvider>();
+            services.AddSingleton<ITaxiCarDataSeederService, TaxiCarDataSeederService>();
+            services.AddSingleton<ITaxiCarCRUDService, TaxiCarCRUDService>();
+        }
+    }
+}
