8a162986e96e04d2c0a2ca9f90b157c7356a251c
AbelMiklos <147874802+AbelMiklos@users.noreply.github.com>
2024-12-01 19:52:45 +0100

    Created search service (#4)

---
+++ src/GMYEL8_HSZF_2024251/GMYEL8_HSZF_2024251.Application/Implementations/SearchServices/ITaxiCarSearchService.cs
@@ -0,0 +1,18 @@
+﻿using GMYEL8_HSZF_2024251.Model.Entities;
+using GMYEL8_HSZF_2024251.Model.Search.Criterias;
+using GMYEL8_HSZF_2024251.Model.Search.Results;
+
+namespace GMYEL8_HSZF_2024251.Application.Implementations.SearchServices;
+
+/// <summary>
+///     Service for searching TaxiCar entities.
+/// </summary>
+public interface ITaxiCarSearchService
+{
+    /// <summary>
+    ///    Searches TaxiCar entities based on the provided criteria.
+    /// </summary>
+    /// <param name="criteria">The criteria to filter the TaxiCar entities.</param>
+    /// <returns>A paginated list of TaxiCar entities that match the search criteria.</returns>
+    Task<PaginatedResult<TaxiCar>> SearchTaxiCarsAsync(TaxiCarSearchCriteria criteria);
+}
---
+++ src/GMYEL8_HSZF_2024251/GMYEL8_HSZF_2024251.Model/Search/Results/PaginatedResult.cs
@@ -0,0 +1,14 @@
+﻿namespace GMYEL8_HSZF_2024251.Model.Search.Results;
+
+public class PaginatedResult<T>
+{
+    public List<T> Items { get; set; } = [];
+
+    public int TotalCount { get; set; }
+
+    public int CurrentPage { get; set; }
+
+    public int PageSize { get; set; }
+
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+}
