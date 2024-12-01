24c9e239b3e4da366dfbc27a7456a495ff61f850
Ábel Miklós <abel.miklos@stud.uni-obuda.hu>
2024-12-01 19:51:26 +0100

    Created search service

---
+++ src/GMYEL8_HSZF_2024251/GMYEL8_HSZF_2024251.Application/Definitions/SearchServices/TaxiCarSearchService.cs
@@ -0,0 +1,45 @@
+﻿using GMYEL8_HSZF_2024251.Application.Implementations.SearchServices;
+using GMYEL8_HSZF_2024251.Model.Entities;
+using GMYEL8_HSZF_2024251.Model.Search.Criterias;
+using GMYEL8_HSZF_2024251.Model.Search.Results;
+using GMYEL8_HSZF_2024251.Persistence.MsSql.DataProviders.Definitions;
+
+namespace GMYEL8_HSZF_2024251.Application.Definitions.SearchServices;
+
+/// <inheritdoc cref="ITaxiCarSearchService"/>
+public class TaxiCarSearchService(ITaxiCarServiceDataProvider taxiCarDataProvider) : ITaxiCarSearchService
+{
+    private readonly ITaxiCarServiceDataProvider _taxiCarDataProvider = taxiCarDataProvider;
+
+    public async Task<PaginatedResult<TaxiCar>> SearchTaxiCarsAsync(TaxiCarSearchCriteria criteria)
+    {
+        var query = _taxiCarDataProvider.ReadAll();
+
+        if (!string.IsNullOrEmpty(criteria.LicensePlate))
+        {
+            query = query.Where(t => t.LicensePlate == criteria.LicensePlate);
+        }
+
+        if (!string.IsNullOrEmpty(criteria.Driver))
+        {
+            query = query.Where(t => t.Driver == criteria.Driver);
+        }
+
+        int totalCount = query.Count();
+
+        var items = query
+            .Skip((criteria.PageNumber - 1) * criteria.PageSize)
+            .Take(criteria.PageSize)
+            .ToList();
+
+        var result = new PaginatedResult<TaxiCar>
+        {
+            Items = items,
+            TotalCount = totalCount,
+            CurrentPage = criteria.PageNumber,
+            PageSize = criteria.PageSize
+        };
+
+        return await Task.FromResult(result);
+    }
+}
