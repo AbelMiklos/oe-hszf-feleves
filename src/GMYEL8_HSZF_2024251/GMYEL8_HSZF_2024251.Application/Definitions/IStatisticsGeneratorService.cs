3ba136dba3b6ee2f4300133da3da547c930250c2
Ábel Miklós Balázs <miklosbalazs.abel@griffsoft.hu>
2024-12-03 12:34:29 +0100

    Renamed StatisticsService to mark the export task

--- src/GMYEL8_HSZF_2024251/GMYEL8_HSZF_2024251.Application/Definitions/IStatisticsGeneratorService.cs
+++ src/GMYEL8_HSZF_2024251/GMYEL8_HSZF_2024251.Application/Definitions/IStatisticsGeneratorService.cs
@@ -6,24 +6,25 @@ namespace GMYEL8_HSZF_2024251.Application.Definitions;
 /// <summary>
 ///    Provides statistics for <see cref="TaxiCar"/> and <see cref="Service"/> entities.
 /// </summary>
-public interface IStatisticsService
+public interface IStatisticsGeneratorService
 {
     /// <summary>
     ///     Gets the number of trips less than 10 kilometers for each car.
     /// </summary>
-    /// <param name="maxDistance">The maximum distance of a trip to be considered short.</param>
+    /// <param name="outpuPath"></param>
     /// <returns>A dictionary where the key is the <see cref="TaxiCar"/> and the value is the number of trips less than 10 kilometers.</returns>
-    Task<Dictionary<TaxiCar, int>> GetShortTripsCountPerCarAsync(int maxDistance);
+    /// <param name="maxDistance">The maximum distance of a trip to be considered short.</param>
+    Task GetShortTripsCountPerCarAsync(string? outpuPath, int maxDistance);
 
     /// <summary>
     ///     Gets the average trip distance, the longest trip, and the shortest trip for each car.
     /// </summary>
     /// <returns>A dictionary where the key is the <see cref="TaxiCar"/> and the value is a <see cref="TaxiCarServiceStatistic"/> containing the average trip distance, the longest trip, and the shortest trip.</returns>
-    Task<Dictionary<TaxiCar, TaxiCarServiceStatistic>> GetTripStatisticsPerCarAsync();
+    Task GetTripStatisticsPerCarAsync(string? outputPath);
 
     /// <summary>
     ///    Gets the most frequent destination for each car.
     /// </summary>
     /// <returns>A dictionary where the key is the <see cref="TaxiCar"/> and the value is the name of the most frequent destination.</returns>
-    Task<Dictionary<TaxiCar, string>> GetMostFrequentDestinationPerCarAsync();    
+    Task GetMostFrequentDestinationPerCarAsync(string? outputPath);    
 }
