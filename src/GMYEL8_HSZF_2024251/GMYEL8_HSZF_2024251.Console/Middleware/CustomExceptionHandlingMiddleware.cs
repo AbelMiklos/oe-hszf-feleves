9d8ae0f9b8d83169c4b7fa9f2e5f6d5e4d7b13e4
Ábel Miklós <abel.miklos@stud.uni-obuda.hu>
2024-12-08 20:36:35 +0100

    Fixed exception handling middleware async issues

--- src/GMYEL8_HSZF_2024251/GMYEL8_HSZF_2024251.Console/Middleware/CustomExceptionHandlingMiddleware.cs
+++ src/GMYEL8_HSZF_2024251/GMYEL8_HSZF_2024251.Console/Middleware/CustomExceptionHandlingMiddleware.cs
@@ -11,7 +11,7 @@ public class CustomExceptionHandlingMiddleware : ICustomMiddleware
     {
         try
         {
-            await next();
+            Task.WaitAll(next());
         }
         catch (BusinessException ex)
         {
